23e15f8464f58a32b32e1ec6f811e8698073bcca
Jack Kooley <jkooley@gmail.com>
2019-02-06 10:30:33 -0800

    updated MultJack

--- src/MultJack.java
+++ src/MultJack.java
@@ -1,7 +1,7 @@
 
 public class MultJack {
 	public static void main(String[] args) {
-		int num1 = 5;
+		int num1 = 2;
 		int num2 = 6;
 		int product = num1 * num2;
 		System.out.println("Product: " + product);
