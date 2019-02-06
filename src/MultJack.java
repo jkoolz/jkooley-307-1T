975f2a01e500c1585239af3192d0ad4791825b55
Jack Kooley <jkooley@gmail.com>
2019-02-06 10:27:45 -0800

    adding everything

---
+++ src/MultJack.java
@@ -0,0 +1,9 @@
+
+public class MultJack {
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 6;
+		int product = num1 * num2;
+		System.out.println("Product: " + product);
+	}
+}
